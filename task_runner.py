3cb13a49bbba188d4faeaca6bd137577855dfed9
迷路的肖恩 <122790803+incompletecactus@users.noreply.github.com>
2025-10-16 12:22:33 +0800

    Update task_runner.py

--- task_runner.py
+++ task_runner.py
@@ -4,7 +4,7 @@ import requests
 import hashlib
 
 #用发的获取工具获取tokens和查询
-tokens = "ead57444f0ff46b15a101577fbcbd2eb&241188ae3508bc5883bf68f79ea9c509"
+tokens = "ec522074d3aa17a9dc7ffa7f09ee4c0d"
 #多账号用&分隔，token1&token2
 tokens = tokens.split("&")
 #https://tool.ip138.com/useragent/
